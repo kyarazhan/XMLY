df1384f451d9c6c90a402bacde584ddf8dd6a8f7
kyarazhan <shangrilas@live.com>
2020-06-07 10:39:13 +0800

    小的修复

--- ximalaya.py
+++ ximalaya.py
@@ -30,17 +30,26 @@ def download_media(id_,name):
 
 #清除存储命名中的文件非法字符
 def validateTitle(title):
-    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
-    # 替换为空格
-    new_title = re.sub(rstr, " ", title)
+    # '/ \ : * ? " < > |'
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"
+    #多空格
+    rstr_1 = r"  "
+    rstr_2 = r"   "
+    # 非法字符替换为空格
+    new_title_1 = re.sub(rstr, " ", title)
+    # 双空格替换为单空格
+    new_title_2 = re.sub(rstr_2, " ", new_title_1)
+    # 三空格替换为单空格
+    new_title = re.sub(rstr_1, " ", new_title_2)
     return new_title
 
+"""
 def serial(title):
 
     for serial_ in range(1,main_.serial_1):
         new_title = str(serial_)+title
         return new_title
-
+"""
 
 def main_():
     link = input("请输入地址栏链接:")
@@ -51,9 +60,11 @@ def main_():
         sel = parsel.Selector(response.text)
         a_s = sel.css('.sound-list ul li a')
         for a in a_s[:30]:
+            #获取文件名
             title = a.css('a::attr(title)').get()
             #调用清除存储命名中的文件非法字符的方法
             title = validateTitle(title)
+            #获取文件后半链接
             url = a.css('a::attr(href)').get()
 
             id_ = url.split('/')[-1]
