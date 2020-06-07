d47963978d3c8dc6507467af11c3b25658593bc0
kyarazhan <shangrilas@live.com>
2020-06-07 10:07:50 +0800

    小的修复

--- ximalaya.py
+++ ximalaya.py
@@ -19,7 +19,7 @@ def download_media(id_,name):
 #    pprint.pprint(data)
     #键值对
     src = data['data']['src']
-#    print(src)
+    print(src)
 
    #根据网址 请求服务器 拿到数据
     response = requests.get(src)
@@ -31,11 +31,11 @@ def download_media(id_,name):
 #清除存储命名中的文件非法字符
 def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
-    new_title = re.sub(rstr, "_", title)  # 替换为下划线
+    # 替换为空格
+    new_title = re.sub(rstr, " ", title)
     return new_title
 
 def serial(title):
-
 
     for serial_ in range(1,main_.serial_1):
         new_title = str(serial_)+title
@@ -45,17 +45,15 @@ def serial(title):
 def main_():
     link = input("请输入地址栏链接:")
     page_ = int(input("请输入总共页数:"))+1
-#    serial_1 = int(input("请输入总章节数"))
     for page in range(1, page_):
         response = requests.get(link+'p'+str(page)+'/', headers=headers)
-        #    print(response.text)
+#        print(response.text)
         sel = parsel.Selector(response.text)
         a_s = sel.css('.sound-list ul li a')
         for a in a_s[:30]:
             title = a.css('a::attr(title)').get()
             #调用清除存储命名中的文件非法字符的方法
             title = validateTitle(title)
-#            title = serial(title)
             url = a.css('a::attr(href)').get()
 
             id_ = url.split('/')[-1]
