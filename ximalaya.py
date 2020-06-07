e69f45d4e003efe482f53240eb5fdb4be16b2740
kyarazhan <shangrilas@live.com>
2020-06-07 09:40:35 +0800

    修复死循环

--- ximalaya.py
+++ ximalaya.py
@@ -34,19 +34,28 @@ def validateTitle(title):
     new_title = re.sub(rstr, "_", title)  # 替换为下划线
     return new_title
 
+def serial(title):
+
+
+    for serial_ in range(1,main_.serial_1):
+        new_title = str(serial_)+title
+        return new_title
+
 
 def main_():
     link = input("请输入地址栏链接:")
     page_ = int(input("请输入总共页数:"))+1
+#    serial_1 = int(input("请输入总章节数"))
     for page in range(1, page_):
-        response = requests.get(link, headers=headers)
+        response = requests.get(link+'p'+str(page)+'/', headers=headers)
         #    print(response.text)
         sel = parsel.Selector(response.text)
         a_s = sel.css('.sound-list ul li a')
         for a in a_s[:30]:
             title = a.css('a::attr(title)').get()
-            #调用清除存储命名中的文件非法字符方法
+            #调用清除存储命名中的文件非法字符的方法
             title = validateTitle(title)
+#            title = serial(title)
             url = a.css('a::attr(href)').get()
 
             id_ = url.split('/')[-1]
